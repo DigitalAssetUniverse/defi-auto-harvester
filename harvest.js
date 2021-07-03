806024320c518ac8fe805df087fdbb5fc4187841
Amarjeet Lokhande <amarjeet.lokhande@anonimous.co.in>
2021-07-03 21:33:48 +0530

    Changed method for harvest from deposit with 0 to withdraw with 0

--- harvest.js
+++ harvest.js
@@ -14,6 +14,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(wallet.rpc));
 var abi = wallet.contractABI;
 const maticAddress = wallet.matic;
 var maticContract = new web3.eth.Contract(abi, maticAddress);
+var toContract = new web3.eth.Contract(abi, wallet.toContract);
 var farmMCabi = wallet.farmMCabi;
 var stakingAdd = wallet.farmMCAdd;
 stakingContract = new web3.eth.Contract(farmMCabi, stakingAdd);
@@ -29,7 +30,16 @@ var maticDecimal = 18;
 var harvestThreshold = wallet.harvestThreshold;
 var sellThreshold = wallet.sellThreshold;
 async function contractInfo() {
+	var count = await web3.eth.getTransactionCount(account,'pending');
+		count = count ;
+        console.log(count);
+		
     try {
+		console.log(wallet.pooledTokens.length);
+		var name = await farmTokenContact.methods.name().call();
+        console.log('The token name is: ' + name);
+		 name = await toContract.methods.name().call();
+        console.log('The token name is: ' + name);
         farmDecimal = await farmTokenContact.methods.decimals().call();
         console.log('The token decimal is: ' + farmDecimal);
         maticDecimal = await maticContract.methods.decimals().call();
@@ -39,11 +49,12 @@ async function contractInfo() {
                 var pid = await stakingContract.methods.poolInfo(p).call();
                 console.log(pid.lpToken);
                 if (!pid) continue;
-                for (var i = 0; i < wallet.pooledTokens.length; i++) {
-                    if (pid.lpToken.trim().toLowerCase() === wallet.pooledTokens[i].trim().toLowerCase()) {
-                        poolIDS.push(p);
-                    }
-                }
+				poolIDS.push(p);
+                // for (var i = 0; i < wallet.pooledTokens.length; i++) {
+                    // if (pid.lpToken.trim().toLowerCase() === wallet.pooledTokens[i].trim().toLowerCase()) {
+                        // poolIDS.push(p);
+                    // }
+                // }
             } catch (ex) {
                 //console.log(ex)
             }
@@ -55,18 +66,25 @@ async function contractInfo() {
     }
 }
 async function harvest() {
+	try{
+	var count = await web3.eth.getTransactionCount(account,'pending');
+		count = count ;
+        console.log(count);
+	var fDec = parseInt(farmDecimal)+2 ;
+	console.log(fDec);
     var tokenHarvestThreshold = 0;
     var tokenSellThreshold = 0;
-    var price = await getPrice(1);
+    var price = await getPrice(1, maticAddress);
     tokenHarvestThreshold = harvestThreshold / price[2];
-    tokenHarvestThreshold = getWeiTokens(farmDecimal, tokenHarvestThreshold / poolIDS.length);
+    tokenHarvestThreshold = getWeiTokens(farmDecimal, tokenHarvestThreshold);
     tokenSellThreshold = getWeiTokens(farmDecimal, sellThreshold / price[2]);
-    console.log(tokenHarvestThreshold, tokenSellThreshold);
+    console.log(getReverseWeiTokens(tokenHarvestThreshold,farmDecimal).toFixed(fDec),
+	getReverseWeiTokens(tokenSellThreshold,farmDecimal).toFixed(fDec)	);
     for (var i = 0; i < poolIDS.length; i++) {
         try {
             var poolId = poolIDS[i];
             var pendingBone = await stakingContract.methods.pendingBone(poolId, account).call();
-            console.log("pendingBone - " + pendingBone);
+            console.log("pendingBone - " + getReverseWeiTokens(pendingBone,farmDecimal).toFixed(fDec));
             if (pendingBone > tokenHarvestThreshold) {
                 console.log("Harvesting for - " + poolId + " - " + pendingBone);
                 await sendHarvest(poolId);
@@ -78,28 +96,39 @@ async function harvest() {
     }
     try {
         var balance = await farmTokenContact.methods.balanceOf(account).call();
-        console.log("Balance " + balance, getWeiTokens(farmDecimal, balance));
+        console.log("Balance " + balance, getReverseWeiTokens(balance,farmDecimal).toFixed(fDec));
         if (balance > tokenSellThreshold) {
             var allowanceResult = await checkAndApprove(farmTokenContact, farmTokenAdd,
                 qsRouterAdd, balance);
             if (!allowanceResult) return { "approval": allowanceResult };
-            price = await getPrice(balance);
+            price = await getPrice(balance,wallet.toContract);
             console.log(price);
-            var buyResult = await sendBuy(balance, price[1]);
+            var buyResult = await sendBuy(balance, price[1], wallet.toContract);
         }
     } catch (ex) {
         console.log(ex);
     }
+	}
+	catch(ex)
+	{
+		console.log(ex);
+	}
     setTimeout(function() {
         harvest();
     }, wallet.delayBetweenHarvests * 1000);
 }
 async function sendHarvest(poolId) {
-    var data = stakingContract.methods.deposit(
+	try{
+    var data = stakingContract.methods.withdraw(
         web3.utils.toHex(poolId), web3.utils.toHex(0));
     console.log("signing tx");
     var result = await sendTransaction(stakingAdd, data,
         web3.utils.toHex(0));
+	}
+	catch(ex)
+	{
+		console.log(ex);
+	}
 }
 async function checkAndApprove(contract, contractAddress, qsRouterAdd, amount) {
     try {
@@ -109,8 +138,8 @@ async function checkAndApprove(contract, contractAddress, qsRouterAdd, amount) {
             console.log('Allowance required is ' + amount);
             if(allowance <= amount)
             {
-				var data = contract.methods.approve(qsRouterAdd, amount*2);
-				await sendTransaction(contractAddress,data,web3.utils.toHex(0));
+				// var data = contract.methods.approve(qsRouterAdd, amount*2);
+				// await sendTransaction(contractAddress,data,web3.utils.toHex(0));
             }
             return true;
         } else {
@@ -121,9 +150,9 @@ async function checkAndApprove(contract, contractAddress, qsRouterAdd, amount) {
         return false;
     }
 }
-async function getPrice(inToken) {
+async function getPrice(inToken, toToken) {
     if (inToken == 0) return [0, 0, 0];
-    var amount = await qsRouter.methods.getAmountsOut(inToken, [farmTokenAdd, maticAddress]).call();
+    var amount = await qsRouter.methods.getAmountsOut(inToken, [farmTokenAdd, toToken]).call();
     if (!amount) {
         return false;
     }
@@ -160,12 +189,12 @@ function getTime() {
         currentdate.getSeconds();
     return datetime + " - ";
 }
-async function sendBuy(inAmount, outAmount) {
+async function sendBuy(inAmount, outAmount, toContract) {
     console.log(inAmount, outAmount);
     var data = qsRouter.methods.swapExactTokensForTokens(
         web3.utils.toHex(inAmount),
         web3.utils.toHex(outAmount),
-        [farmTokenAdd, maticAddress],
+        [farmTokenAdd, toContract],
         account,
         web3.utils.toHex(Math.round(Date.now() / 1000) + 60 * 10),
     );
@@ -175,9 +204,16 @@ async function sendBuy(inAmount, outAmount) {
         web3.utils.toHex(0));
     return result;
 }
+var nonce = wallet.nonce;
 async function sendTransaction(toAddress, data, amountToBuyWith) {
     try {
-		var count = await web3.eth.getTransactionCount(account);
+		var count = await web3.eth.getTransactionCount(account,'pending');
+		if(count < nonce)
+		{
+			count = nonce;
+			nonce++;			
+		}
+		
         console.log(count);
         var gasPrice = await web3.eth.getGasPrice();
 		var gwei = wallet.gwei; 
